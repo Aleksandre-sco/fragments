bb40ca7813235f3e303890468d241a7cfc37bbc2
Aleksandre-sco <aleksandre.kapanadze.2@btu.edu.ge>
2020-12-07 17:40:55 +0400

    Initial commit

---
+++ app/src/main/java/com/example/fragments/fragments/NotificationFragment.kt
@@ -0,0 +1,23 @@
+package com.example.fragments.fragments
+
+import android.app.Notification
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.findNavController
+import com.example.fragments.R
+
+class NotificationFragment : Fragment (R.layout.fragment_notification) {
+
+    private lateinit var textView: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView = view.findViewById(R.id.textView2)
+
+        textView.text = NotificationFragmentArgs.fromBundle(requireArguments()).amount.toString()
+    }
+}
